5ab49fe6d5c91db685c527fa5c00c7d08aaf125d
Alexander Rukhlin <stinger15211@yandex.ru>
2019-03-25 22:19:17 +0300

    com

---
+++ script.js
@@ -0,0 +1,147 @@
+$(document).ready(begAnimation);
+
+$(window).resize(function() {
+	console.log("resize");
+	location.reload(false);
+});
+
+//Начальная анимация 
+var begAnimation = setTimeout(function() {
+	$(".begAnim").css("margin-left", "0");
+	$(".begAnim").css("opacity", "1");
+	$(".header ul").css("padding-right", "0");
+	$(".header ul").css("opacity", "1");
+	$("#woman").css("opacity", "1");
+}, 400);
+
+//Открытие всплывающей формы регистрации
+var regClean1 = document.getElementById("block1");
+var regClean2 = document.getElementById("block2");
+var regClean3 = document.getElementById("block3");
+var regClean4 = document.getElementById("cardbox");
+var regClean = [];
+var tempClean;
+tempClean = regClean1.getElementsByTagName("*");
+console.log(tempClean);
+
+var reg = document.getElementById("block8");
+$("#phone").click(function() {
+	$("body::-webkit-scrollbar").css("width", "0");
+	$("#block8").css("display", "block");
+	$("#block8").css("visibility", "visible");
+	$("#block8").css("opacity", "1");
+	$("#block1, #block2").css("filter", "blur(5px)");
+});
+$(".card-button-div").click(function() {
+	$('body,html').animate({scrollTop: 0}, 300);
+	$("#block8").css("display", "block");
+	$("#block8").css("visibility", "visible");
+	$("#block8").css("opacity", "1");
+	$("#block1, #block2").css("filter", "blur(5px)");
+});
+
+$(function() {
+   $("#block1, #block2, #block8").mousewheel(function(evt, del) {
+      if (getComputedStyle(reg).display == "block") {
+      	$("::-webkit-scrollbar").css("display", "none");
+      	return false;
+      }
+   });
+});
+
+
+
+//Закрытие всплывающей формы регистрации
+window.onclick = function(event) {
+	if (event.target == regClean1 || event.target == regClean2 || event.target == regClean3 || event.target == regClean4) {
+		reg.style.opacity = "0";
+		reg.style.visibility = "hidden";
+		reg.style.display = "none";
+		$("#block8").removeClass("reg2");
+		$("#block1, #block2, #block3, #block4, #block5, #block6, #block7").css("filter", "blur(0)");
+		console.log("done");
+		console.log(event.target);
+	}
+}
+
+
+//Скорость скролла блока 2 
+var lambd = 0;
+var curWidth = getComputedStyle(document.body).width;
+curWidth = curWidth.substring(0, curWidth.length - 2)
+if (curWidth > 1500) {
+	lambd = 30;
+}
+else {
+	lambd = 15;
+}
+console.log(curWidth);
+
+
+$(function() {
+   $("#cardbox").mousewheel(function(evt, del) {
+      this.scrollLeft -= (del * lambd);
+      console.log(evt);
+      evt.preventDefault();
+   });
+});
+
+//Запрет прокрутки на слайдере 2
+$(function() {
+   $(".slider").mousewheel(function(evt, del) {
+      evt.preventDefault();
+   });
+});
+
+
+//Определение вида слайдера 2 
+if (curWidth < 1000) {
+	$("#block4_mobile").css("display", "block");
+	$("#block4").css("display", "none");
+}
+else {
+	$("#block4").css("display", "block");
+	$("#block4_mobile").css("display", "none");
+}
+ 
+
+//Прокрутка по ссылкам
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+
+
+//Кнопка "Подробнее" на слайдере 1 
+$(".square .card-content").mouseover(function(event) {
+	$(this).addClass("active");
+});
+$(".square .card-content").mouseout(function() {
+	$("div").removeClass("active");
+});
+$(".card-button-div").mouseover(function(event) {
+	$(this).prev().addClass("active");
+});
+$(".card-button-div").mouseout(function() {
+	$("div").removeClass("active");
+});
+
+
+$(".rect .half").mouseover(function() {
+	$(this).addClass("active");
+	console.log(1);
+});
+$(".rect .half").mouseout(function() {
+	$("div").removeClass("active");
+});
---
+++ slider.js
@@ -0,0 +1,138 @@
+'use strict';
+    var multiItemSlider = (function () {
+      return function (selector, config) {
+        var
+          _mainElement = document.querySelector(selector), // основный элемент блока
+          _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), // обертка для .slider-item
+          _sliderItems = _mainElement.querySelectorAll('.slider__item'), // элементы (.slider-item)
+          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
+          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width) + 24, // ширина одного элемента    
+          _positionLeftItem = 0, // позиция левого активного элемента
+          _transform = 0, // значение транфсофрмации .slider_wrapper
+          _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+          _items = []; // массив элементов
+            
+        // наполнение массива _items
+        _sliderItems.forEach(function (item, index) {
+          _items.push({ item: item, position: index, transform: 0 });
+        });
+
+        var position = {
+          getItemMin: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+              if (item.position < _items[indexItem].position) {
+                indexItem = index;
+              }
+            });
+            return indexItem;
+          },
+          getItemMax: function () {
+            var indexItem = 0;
+            _items.forEach(function (item, index) {
+              if (item.position > _items[indexItem].position) {
+                indexItem = index;
+              }
+            });
+            return indexItem;
+          },
+          getMin: function () {
+            return _items[position.getItemMin()].position;
+          },
+          getMax: function () {
+            return _items[position.getItemMax()].position;
+          }
+        }
+
+        var _transformItem = function (direction) {
+          var nextItem;
+          if (direction === 'right') {
+            _positionLeftItem++;
+            if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+              nextItem = position.getItemMin();
+              _items[nextItem].position = position.getMax() + 1;
+              _items[nextItem].transform += _items.length * 100;
+              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
+            _transform -= _step;
+          }
+          if (direction === 'left') {
+            _positionLeftItem--;
+            if (_positionLeftItem < position.getMin()) {
+              nextItem = position.getItemMax();
+              _items[nextItem].position = position.getMin() - 1;
+              _items[nextItem].transform -= _items.length * 100;
+              _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+            }
+            _transform += _step;
+          }
+          _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+        }
+
+        function wheel(event) {
+            var delta; // Направление скролла
+            // -1 - скролл вниз
+            // 1  - скролл вверх
+            event = event || window.event;
+            // Opera и IE работают со свойством wheelDelta
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                // В Опере значение wheelDelta такое же, но с противоположным знаком
+                if (window.opera) delta = -delta;
+            // В реализации Gecko получим свойство detail
+            } else if (event.detail) {
+                delta = -event.detail / 3;
+            }
+            // Запрещаем обработку события браузером по умолчанию
+            if (event.preventDefault)  event.preventDefault();
+            event.returnValue = false;
+            console.log(delta);
+            return delta;
+        }
+
+        
+        // обработчик события click для скролла
+        var _controlClick = function (e) {
+          var direction = (wheel() < 0) ? 'right' : 'left';
+          console.log(direction);
+          e.preventDefault();
+          _transformItem(direction);
+        };
+
+        // функции для обработки событий через интервал
+        function _checkTimer(func, interval) {
+            var lastCall = 0;
+            return function() {
+                var now = Date.now();
+                if (lastCall + interval < now) {
+                    lastCall = now;
+                    console.log(lastCall);
+                    return func.apply(this, arguments);
+                }
+            };
+        }
+
+        var _setUpListeners = function() {
+          _sliderWrapper.addEventListener("mousewheel", _checkTimer(_controlClick, 800));
+          _sliderWrapper.addEventListener("touchstart", _checkTimer(_controlClick, 800));
+          /*_sliderWrapper.addEventListener("click", _checkTimer(_controlClick, 800));*/
+          _sliderWrapper.addEventListener("DOMMouseScroll", _checkTimer(_controlClick, 800));
+          _sliderWrapper.addEventListener("wheel", _checkTimer(_controlClick, 800));
+        }
+
+        // инициализация
+        _setUpListeners();
+
+        return {
+          right: function () { // метод right
+            _transformItem('right');
+          },
+          left: function () { // метод left
+            _transformItem('left');
+          }
+        }
+
+      }
+    }());
+
+    var slider = multiItemSlider('.slider')
